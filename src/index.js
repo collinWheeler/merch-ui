d6912ecb818089ad263d361174b1cf39d5f16d13
collinWheeler <collinWheeler@users.noreply.github.com>
2021-11-23 21:27:30 -0500

    Created login page and routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import MainPage from "./main/MainPage";
+import LoginPage from "./login/LoginPage";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router>
+        <Routes>
+            <Route path="/" element={<MainPage/>}/>
+            <Route exact path="/login" element={<LoginPage/>}/>
+        </Routes>
+    </Router>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/login/LoginPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class LoginPage extends React.Component {
+    static propTypes = {};
+
+    state = {};
+
+    render() {
+        return <>Login Here</>;
+    }
+}
+
+export default LoginPage;
